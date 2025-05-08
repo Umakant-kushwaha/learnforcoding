c387a891160e3ec84369aa3688b241196a8feaad
Umakant-kushwaha <uk9255690@gmail.com>
2025-05-08 01:02:17 -1200

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,17 @@
+a = int(input("first num"))
+x = input("* + - /")
+b = int (input("sec num"))
+if (x == "*"):
+    c = a*b
+    print("ans =", c)
+elif (x == "+"):
+    c = a+b
+    print("ans =", c)
+elif (x == "-"):
+    c = a-b
+    print("ans =", c)
+elif (x == "/"):
+    c = a/b
+    print("ans =", c)
+    
+
