abd14a75e9a5fbdadf442304c01590895616647e
Umakant-kushwaha <uk9255690@gmail.com>
2025-05-08 01:03:36 -1200

    Add files via upload

---
+++ day8.py
@@ -0,0 +1,23 @@
+# a = 10
+# b = 15
+# c = a 
+# a = b
+# b = c
+# print(a)
+# print(b)
+
+# a = 10
+# b = 15
+# a = a+b
+# b = a-b
+# a = a-b
+# print(a)
+# print(b)
+
+Name = input("name")
+age = int(input("age"))
+address = input("address")
+print("name")
+print("age")
+print("address")
+print("name , age , address")
