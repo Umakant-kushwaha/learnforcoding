43f9083b05ee14ff34545dbd9e0b491d60fa960c
Umakant-kushwaha <uk9255690@gmail.com>
2025-05-08 01:09:09 -1200

    Add files via upload

---
+++ day u.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	// relation operator
+    int a = 10;
+    int b = 8;
+    int c = a < b;
+	printf("%d\n",c);
+	
+	// relation operator
+	int a1 = 5;
+	int b2 = 2;
+	int c3 = a1 > b2;
+	printf("%d\n",c);
+	
+	return(0);
+}
+	
+
