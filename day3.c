01c78d9736005e5becf0f639c7a23063ca12d631
Umakant-kushwaha <uk9255690@gmail.com>
2025-05-08 01:12:25 -1200

    Add files via upload

---
+++ day3.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+int main(void){
+	// arithmetic operator
+	// + - * / %
+	// arithmetic operation addition
+	int a = 10;
+	int b = 20;
+	int c = a+b;
+	printf("c = %d\n", c);
+	
+	// arithmetic operation subtraction
+	int d = b - a;
+	printf("d = %d\n", d);
+	
+	// arithmetic operation multiplication
+	int e = a*b;
+	printf("e = %d\n", e);
+	
+	// aerithmetic operation division
+	float num1 = 10.0;
+	float num2 = 3;
+	float num3 = num1/num2;   
+	printf("num3 %f\n", num3);
+	
+	// arithmetic operation modulas
+	int num4 = 10;
+	int num5 = 3;
+	int num6 = num4 % num5;
+	printf("num6 = %d\n", num6);
+	
+	// arithmetic operation post increment
+	int num7 = 15;
+	printf("num7 = %d\n", num7++);
+	printf("num7 = %d\n", num7);
+	
+	// arithmetic operation decrement
+	int num8 = 10;
+	printf("num8 = %d\n", num8--);
+	printf("num8 %d\n",num8)
+	
+	// arithmetic operationpre pre increment
+    int num9 = 10;
+	printf("num9 = %d\n",++num9);
+	printf("num9 = %d\n", num9);
+	
+	// arithmetic operation pre decrement
+	int num10 = 10;
+	printf("num10 = %d\n",--num10);
+	printf("num10 = %d\n", unm10);
+}
