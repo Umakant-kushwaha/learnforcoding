33ab155fcec5c6be4b4761e9ed70f6b0cba2251e
Umakant-kushwaha <uk9255690@gmail.com>
2025-05-08 01:04:49 -1200

    Add files via upload

---
+++ day9.py
@@ -0,0 +1,7 @@
+n1 = int(input("enter your first number:"))
+n2 = int(input("enter your second number:"))
+n3 = int(input("enter your third number:"))
+sum = n1+n2+n3
+average = sum / 3
+print ("sum of three numbers is:", sum )
+print ("average of three numbers is:", average)
