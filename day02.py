f5b57e1c35bf69d2cdeb1b4e44e8e0363f92cb5d
Umakant-kushwaha <uk9255690@gmail.com>
2025-05-08 01:01:13 -1200

    Add files via upload

---
+++ day02.py
@@ -0,0 +1,13 @@
+umakant = ["aman", "ankit", "rishi"]
+for x in umakant:
+    print(x)
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    if x == "banana":
+        break
+umakant = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        break
+    print(x)    
