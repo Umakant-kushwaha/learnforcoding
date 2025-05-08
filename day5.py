27ab499f6799ab0abffb97f1ed5279584edce5c4
Umakant-kushwaha <uk9255690@gmail.com>
2025-05-08 01:02:38 -1200

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,10 @@
+def my_function(fname, lname):
+    print(fname +" "+ lname)
+
+    my_function("email", "refsens")
+
+def my_function(fname):
+    print(fname + "refsens")
+    my_function("email")
+    my_function("tobias")
+    my_function("linus")
