e570e51327d173762cc54f0300e6700698c3ef8b
Umakant-kushwaha <uk9255690@gmail.com>
2025-05-09 05:12:40 -0700

    Add files via upload

---
+++ umakant730775.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+//int main() {
+//  int i;
+//
+//  for (i = 1; i < 101; i++) {
+//    printf("%d\n", i);
+//  }
+//  
+//  return 0;
+//}
+
+int main() {
+  int i = 0;
+  
+  while (i < 100) {
+    printf("%d\n", i);
+    i++;
+  }
+  
+  return 0;
+}
