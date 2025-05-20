805d4187a5b44506849ca5021150a217311841cd
Umakant-kushwaha <uk9255690@gmail.com>
2025-05-20 17:42:28 +0530

    Add files via upload

---
+++ xml or lmx  .c
@@ -0,0 +1,23 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main()
+{
+    // declare and initilize character variables
+    char char1 = 'X';
+    char char2 = 'M';
+    char char3 = 'L';
+    
+    // print the orginal reversed charaters
+    printf("the reverse of %c%c%c is %c%c%c\n",
+        char1, char2, char3, 
+        char3, char2, char1);
+        
+        
+      
+        
+        
+        return(0);
+        
+    
+}
+
